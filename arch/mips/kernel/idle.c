ff84914c8f1d4ed758e6c54d1b0c4671b0c8aa40
yangxiaojuan <yangxiaojuan@loongson.cn>
2015-06-04 14:43:48 +0800

    3A2000: add wait instruction support
    Change-Id: Icd62e9cb7c1763d52d1be6bc73b10de3499128f6

--- arch/mips/kernel/idle.c
+++ arch/mips/kernel/idle.c
@@ -172,6 +172,10 @@ void __init check_wait(void)
 	case CPU_XLP:
 		cpu_wait = r4k_wait;
 		break;
+	case CPU_LOONGSON3:
+		if ((read_c0_prid() & 0xf) == PRID_REV_LOONGSON3A2000)
+			cpu_wait = r4k_wait;
+		break;
 
 	case CPU_RM7000:
 		cpu_wait = rm7k_wait_irqoff;
