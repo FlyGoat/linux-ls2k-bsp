1343ba7e2d54c35b197432fba96df889a68a0dbb
Roman Gushchin <klamm@yandex-team.ru>
2015-11-05 18:47:08 -0800

    mm: use only per-device readahead limit
    Maximal readahead size is limited now by two values:
     1) by global 2Mb constant (MAX_READAHEAD in max_sane_readahead())
     2) by configurable per-device value* (bdi->ra_pages)
    
    There are devices, which require custom readahead limit.
    For instance, for RAIDs it's calculated as number of devices
    multiplied by chunk size times 2.
    
    Readahead size can never be larger than bdi->ra_pages * 2 value
    (POSIX_FADV_SEQUNTIAL doubles readahead size).
    
    If so, why do we need two limits?
    I suggest to completely remove this max_sane_readahead() stuff and
    use per-device readahead limit everywhere.
    
    Also, using right readahead size for RAID disks can significantly
    increase i/o performance:
    
    before:
      dd if=/dev/md2 of=/dev/null bs=100M count=100
      100+0 records in
      100+0 records out
      10485760000 bytes (10 GB) copied, 12.9741 s, 808 MB/s
    
    after:
      $ dd if=/dev/md2 of=/dev/null bs=100M count=100
      100+0 records in
      100+0 records out
      10485760000 bytes (10 GB) copied, 8.91317 s, 1.2 GB/s
    
    (It's an 8-disks RAID5 storage).
    
    This patch doesn't change sys_readahead and madvise(MADV_WILLNEED)
    behavior introduced by 6d2be915e589b58 ("mm/readahead.c: fix readahead
    failure for memoryless NUMA nodes and limit readahead pages").
    
    Signed-off-by: Roman Gushchin <klamm@yandex-team.ru>
    Cc: Raghavendra K T <raghavendra.kt@linux.vnet.ibm.com>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Wu Fengguang <fengguang.wu@intel.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: onstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Jamie Iles <jamie.iles@oracle.com>

--- mm/readahead.c
+++ mm/readahead.c
@@ -216,7 +216,7 @@ int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 	if (unlikely(!mapping->a_ops->readpage && !mapping->a_ops->readpages))
 		return -EINVAL;
 
-	nr_to_read = max_sane_readahead(nr_to_read);
+	nr_to_read = min(nr_to_read, inode_to_bdi(mapping->host)->ra_pages);
 	while (nr_to_read) {
 		int err;
 
@@ -235,16 +235,6 @@ int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 		nr_to_read -= this_chunk;
 	}
 	return ret;
-}
-
-#define MAX_READAHEAD   ((512*4096)/PAGE_CACHE_SIZE)
-/*
- * Given a desired number of PAGE_CACHE_SIZE readahead pages, return a
- * sensible upper limit.
- */
-unsigned long max_sane_readahead(unsigned long nr)
-{
-	return min(nr, MAX_READAHEAD);
 }
 
 /*
@@ -400,7 +390,7 @@ ondemand_readahead(struct address_space *mapping,
 		   bool hit_readahead_marker, pgoff_t offset,
 		   unsigned long req_size)
 {
-	unsigned long max = max_sane_readahead(ra->ra_pages);
+	unsigned long max = ra->ra_pages;
 	pgoff_t prev_offset;
 
 	/*
