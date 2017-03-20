b7546b5919bba8767a7e572f282f04cf0c544741
QiaoChong <qiaochong@loongson.cn>
2017-03-20 19:40:27 +0800

    fix ls2k address space.
    Signed-off-by: QiaoChong <qiaochong@loongson.cn>

--- arch/mips/include/asm/mach-loongson2k/spaces.h
+++ arch/mips/include/asm/mach-loongson2k/spaces.h
@@ -3,11 +3,7 @@
 
 #ifndef CAC_BASE
 #if defined(CONFIG_64BIT)
-#if defined(CONFIG_DMA_NONCOHERENT) || defined(CONFIG_CPU_LOONGSON3)
 #define CAC_BASE        _AC(0x9800000000000000, UL)
-#else
-#define CAC_BASE        _AC(0xa800000000000000, UL)
-#endif /* CONFIG_DMA_NONCOHERENT || CONFIG_CPU_LOONGSON3 */
 #endif /* CONFIG_64BIT */
 #endif /* CONFIG_CAC_BASE */
 
