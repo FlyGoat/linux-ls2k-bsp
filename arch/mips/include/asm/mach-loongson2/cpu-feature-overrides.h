e9f6da8154ac2bbd802111f371f6a05acd7bacc2
Huang Pei <huangpei@loongson.cn>
2017-05-26 11:16:32 +0800

    MIPS: add cpu feature overrides for 2k1000
    Change-Id: If045186d66792d4ca8887af8bb815fc9695ce7d2
    Signed-off-by: Huang Pei <huangpei@loongson.cn>

---
+++ arch/mips/include/asm/mach-loongson2/cpu-feature-overrides.h
@@ -0,0 +1,42 @@
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ */
+
+#ifndef __ASM_MACH_LOONGSON2K_FEAUTRE_OVERRIDES_H
+#define __ASM_MACH_LOONGSON2K_FEAUTRE_OVERRIDES_H
+
+#define cpu_has_32fpr		1
+#define cpu_has_3k_cache	0
+#define cpu_has_4k_cache	1
+#define cpu_has_4kex		1
+#define cpu_has_64bits		1
+#define cpu_has_cache_cdex_p	0
+#define cpu_has_cache_cdex_s	0
+#define cpu_has_counter		1
+#define cpu_has_dc_aliases	0
+#define cpu_has_divec		0
+#define cpu_has_ejtag		0
+#define cpu_has_fpu		1
+#define cpu_has_inclusive_pcaches	1
+#define cpu_has_llsc		1
+#define cpu_has_mcheck		0
+#define cpu_has_mdmx		0
+#define cpu_has_mips16		0
+#define cpu_has_mips3d		0
+#define cpu_has_mipsmt		0
+#define cpu_has_smartmips	0
+#define cpu_has_tlb		1
+#define cpu_has_tx39_cache	0
+#define cpu_has_vce		0
+#define cpu_has_veic		0
+#define cpu_has_vint		0
+#define cpu_has_vtag_icache	0
+#define cpu_has_watch		0
+#define cpu_has_local_ebase	1
+#define cpu_has_perf_cntr_intr_bit	1
+#define cpu_has_ic_fills_f_dc	1
+
+#endif
