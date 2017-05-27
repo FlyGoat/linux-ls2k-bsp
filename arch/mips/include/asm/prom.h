621dafb0cf8987e5c0f41e460d9eb6cbde46ab9f
Huang Pei <huangpei@loongson.cn>
2017-05-27 11:29:26 +0800

    of/pci: clean up pci_address_to_pio
    Change-Id: Ib76ef77bdf58cc28fc87dfd19ad678cc3dfaca1f
    Signed-off-by: Huang Pei <huangpei@loongson.cn>

--- arch/mips/include/asm/prom.h
+++ arch/mips/include/asm/prom.h
@@ -22,16 +22,6 @@ extern int early_init_dt_scan_memory_arch(unsigned long node,
 
 extern void device_tree_init(void);
 
-static inline unsigned long pci_address_to_pio(phys_addr_t address)
-{
-	/*
-	 * The ioport address can be directly used by inX() / outX()
-	 */
-	BUG_ON(address > IO_SPACE_LIMIT);
-
-	return (unsigned long) address;
-}
-#define pci_address_to_pio pci_address_to_pio
 
 struct boot_param_header;
 
