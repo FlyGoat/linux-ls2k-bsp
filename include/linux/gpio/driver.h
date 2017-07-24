6ffb141e7dd1eb5b0d56b1ab2fde7f8b60a5e9de
Huang Pei <huangpei@loongson.cn>
2017-07-24 10:12:40 +0800

    gpiolib: import upstream: bb1e88ccb771492ac908ac295ec135efa1d53093
    Change-Id: I0d521347a2ce73574eb43baf04d54d98a7dcd485
    Signed-off-by: Huang Pei <huangpei@loongson.cn>

--- include/linux/gpio/driver.h
+++ include/linux/gpio/driver.h
@@ -11,6 +11,8 @@ struct device_node;
 struct seq_file;
 struct gpio_device;
 
+#ifdef CONFIG_GPIOLIB
+
 /**
  * struct gpio_chip - abstract a GPIO controller
  * @label: for diagnostics
@@ -130,6 +132,12 @@ extern struct gpio_chip *gpiochip_find(void *data,
 int gpiod_lock_as_irq(struct gpio_desc *desc);
 void gpiod_unlock_as_irq(struct gpio_desc *desc);
 
+struct gpio_chip *gpiod_to_chip(const struct gpio_desc *desc);
+
+struct gpio_desc *gpiochip_get_desc(struct gpio_chip *chip,
+				    u16 hwnum);
+
+
 enum gpio_lookup_flags {
 	GPIO_ACTIVE_HIGH = (0 << 0),
 	GPIO_ACTIVE_LOW = (1 << 0),
@@ -183,5 +191,16 @@ struct gpiod_lookup_table {
 }
 
 void gpiod_add_lookup_table(struct gpiod_lookup_table *table);
+
+#else /* CONFIG_GPIOLIB */
+
+static inline struct gpio_chip *gpiod_to_chip(const struct gpio_desc *desc)
+{
+	/* GPIO can never have been requested */
+	WARN_ON(1);
+	return ERR_PTR(-ENODEV);
+}
+
+#endif /* CONFIG_GPIOLIB */
 
 #endif
